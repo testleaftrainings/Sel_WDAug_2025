243d3857b8276e351efd9f98fc0ec47001e93bd0
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-08-06 15:52:44 +0530

    Add files via upload

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstProgram {
+	
+	//type main and do ctrl+space
+	public static void main(String[] args) {
+		
+		//how to print the statement--->type syso and do ctrl+space
+		System.out.println("welcome to testleaf");
+		
+	}
+}
---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		short age=30;
+		int regNo=123456;
+		long phNO=9876503452l;
+		
+		float speed=0.005878787878787f;
+		double capacity=5467.45465655765443;
+		
+		System.out.println("ph no is:"+phNO+"my age is: "+age+"\n"+regNo);
+		
+		char logo='7';
+		
+		String s="hi everyone welcome to testleaf for this selenium course";
+		
+		
+
+	}
+
+}
